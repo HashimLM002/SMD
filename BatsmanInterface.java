f1455d24bc0d5d54f2089612d7a0030c5ba568fd
HashimLM002 <145607057+HashimLM002@users.noreply.github.com>
2023-09-24 02:25:03 +0500

    Delete BatsmanInterface.java

--- BatsmanInterface.java
+++
@@ -1,8 +0,0 @@
-public interface BatsmanInterface extends CricketerInterface {
-   public int getScore();
-   public int getRanking();
-   public String getName();
-   public int getAge();
-   public String getNationality();
-   public void print();
-}
